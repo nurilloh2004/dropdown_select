5129ba9405489c271e67b00ae30d1ae3337b2e46
nurilloh2004 <nurillohpython@gmail.com>
2022-09-18 05:04:02 -0700

    **

--- w/management/commands/load_data.py
+++ w/management/commands/load_data.py
@@ -27,3 +27,20 @@ class Command(BaseCommand):
 
         for module in compsci_modules:
             Module.objects.create(name=module, course=cs)
+    
+    def handle1(self, *args, **kwargs):
+        Room.objects.all()
+        
+        # if not Module.objects.count():
+        #     for module_name in module_names:
+        #         Course.objects.create(name=module_name)
+        md = Module.objects.get(name='AI')
+
+        md_rooms = [
+            'First',
+            'Second',
+            'Third',
+            'Fourth',
+        ]
+        for mdr in md_rooms:
+            Room.objects.create(name=mdr, course=md)
--- w/models.py
+++ w/models.py
@@ -8,3 +8,7 @@ class Course(models.Model):
 class Module(models.Model):
     name = models.CharField(max_length=125)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
+
+class Room(models.Model):
+    name = models.CharField(max_length=125)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='modules')
